88f0f098020a047b34c59bd4746defeec2e18fda
Riccardo Karim Khamaisi <63868396+riccardokhm@users.noreply.github.com>
2026-01-14 17:18:51 +0100

    Control instructions exercises

---
+++ C++/Projects_Part_3_ControlInstructions/Help.cpp
@@ -0,0 +1,34 @@
+//Project to create an online help system for control instructions in cpp.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Welcome to the C++ Control Instructions Help System!" << endl;
+    cout << "Please select a topic to learn more about:" << endl;
+    cout << "1. If Statements" << endl;
+    cout << "2. Switch Statements" << endl;
+
+    int choice;
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        cout << "If Statements:" << endl;
+        cout << "   Syntax: if (condition) { // code to execute }" << endl;
+        cout << "   (Additional) else if/else { // code to execute }" << endl;
+        break;
+
+    case 2:
+        cout << "Switch Statements:" << endl;
+        cout << "   Syntax: switch (variable) { case value1: // code; break; case value2: // code; break; default: // code; }" << endl;
+        break;
+    
+    default:
+        cout << "Invalid choice. Please select a valid topic." << endl;
+        break;
+    }
+
+    return 0;
+}
--- C++/Projects_Part_3_ControlInstructions/Magic_Number.cpp
+++ C++/Projects_Part_3_ControlInstructions/Magic_Number.cpp
@@ -1,4 +1,40 @@
+// Program which mimic the behaviour of a user trying to guess a magic number
 #include <iostream>
 #include <cstdlib>
+#include <random>
 using namespace std;
+
+int main()
+{
+    random_device rd; // Obtain a random number from hardware
+    mt19937 eng(rd()); // Seed the generator
+    uniform_int_distribution<> distr(1, 100); // Define the range
+    const int MAGIC_NUMBER = distr(eng); // The magic number to guess
+    int userGuess = 0; // Variable to store user's guess
+
+    cout << "Welcome to the Magic Number Guessing Game!" << endl;
+    cout << "Try to guess the magic number between 1 and 100." << endl;
+
+    // Loop until the user guesses the correct number
+    while (userGuess != MAGIC_NUMBER)
+    {
+        cout << "Enter your guess: ";
+        cin >> userGuess;
+
+        if (userGuess < MAGIC_NUMBER)
+        {
+            cout << "Too low! Try again." << endl;
+        }
+        else if (userGuess > MAGIC_NUMBER)
+        {
+            cout << "Too high! Try again." << endl;
+        }
+        else
+        {
+            cout << "Congratulations! You've guessed the magic number!" << endl;
+        }
+    }
+
+    return 0;
+}
 
